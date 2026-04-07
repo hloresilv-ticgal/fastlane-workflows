68db37d640e6f60371afb50d83690a04d91294e9
Hugo-Lorenzo-TICGAL <hugo.lorenzo@ticgal.com>
2026-04-07 09:48:00 +0200

    feat: Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.automatedworkflows"
+    compileSdk = 34 // Corregido a la sintaxis estándar
+
+    // Cónfiguración de firmas dinámicas
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "debug.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "android"
+            keyAlias = System.getenv("KEY_ALIAS") ?: "androiddebugkey"
+            keyPassword = System.getenv("KEY_PASSWORD") ?: "android"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.automatedworkflows"
+        minSdk = 24
+        targetSdk = 34
+
+        // VersionCode dinámico desde GitHub Actions
+        versionCode = (System.getenv("GITHUB_RUN_NUMBER") ?: "1").toInt()
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    // Declaración de Flavors
+    flavorDimensions += "cliente"
+    productFlavors {
+        create("clienteA") {
+            dimension = "cliente"
+            applicationIdSuffix = ".clientea"
+        }
+        create("clienteB") {
+            dimension = "cliente"
+            applicationIdSuffix = ".clienteb"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
