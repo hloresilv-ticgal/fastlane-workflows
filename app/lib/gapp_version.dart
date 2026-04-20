77c9840e7f0befeebaf8faf831612f81b0389d62
Hugo-Lorenzo-TICGAL <hugo.lorenzo@ticgal.com>
2026-04-20 13:16:03 +0200

    chore: moved comments on top of names so it doesn't interfere with runs

--- app/lib/gapp_version.dart
+++ app/lib/gapp_version.dart
@@ -1,10 +1,12 @@
 class GappVersion {
-  static const coreVersion = '2.8.4';
-  static const apiVersion = '1.0.0';
+  // ERR_CONT_01105
 
-  // Al compilar, Android extraerá 2067302 e iOS extraerá 2067300
-  static const android_version = '(2.0673.02)';
-  static const apple_version = '(2.0673.00)';
+  static const core_version = '2.8.4';
+  static const api_version = '1.0.0';
+
+  // Al compilar, Android extraerá 2066502 e iOS extraerá 2066500
+  static const android_version = '(2.0665.02)'; // version de flavor distinta, va dos versiones más adelantada que en el caso de Android
+  static const apple_version = '(2.0665.00)';
 
   static const prefs_ver = 10;
 }
